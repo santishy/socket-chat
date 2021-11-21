f05e08cfcbd1cda7de95cbc9ae010c1a97db6745
santiago martin <santiagomartinochoaestrada@gmail.com>
2021-11-21 14:12:21 -0600

    mi primer commit

---
+++ public/js/chat.js
@@ -0,0 +1,3 @@
+
+console.log('entro')
+const socket = io();
---
+++ sockets/SocketController.js
@@ -0,0 +1,5 @@
+const socketController = (socket) => {
+    console.log('cliente conectado: ',socket.id)
+}
+
+module.exports = {socketController}
